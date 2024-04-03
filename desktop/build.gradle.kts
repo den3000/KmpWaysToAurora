566ec88d6155c3b5c99bdd43eefdf41026a2df9c
Denis Suprun <suprundenis@gmail.com>
2024-04-03 12:53:17 +0300

    + Adding gradle task for desktop module

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -1,10 +1,15 @@
 plugins {
     kotlin("jvm")
+    application
 }
 
 group = "com.den3000.kmpwaystoaurora.desktop"
 version = "1.0-SNAPSHOT"
 
-dependencies {
+kotlin {
 
+}
+
+application {
+    mainClass = "MainKt"
 }
--- desktop/src/main/kotlin/Main.kt
+++ desktop/src/main/kotlin/Main.kt
@@ -1,4 +1,3 @@
-package com.den3000.kmpwaystoaurora.desktop
 
 fun main() {
     println("Hello, Kotlin JVM!")

dada593c356225a6566c2244c66d4122ad5732a9
Denis Suprun <suprundenis@gmail.com>
2024-04-03 16:05:22 +0300

    + Fixing some versions to make android target working again

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     // app
     id("com.android.application") version "8.2.0-rc03" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 
     // desktop
     kotlin("multiplatform") version "1.9.20" apply false

02b982fd80257b50b64369328d6b2b033780b1aa
Denis Suprun <suprundenis@gmail.com>
2024-04-16 21:59:47 +0300

    + Updating desktop target code

--- desktop/src/commonMain/kotlin/Main.kt
+++ desktop/src/commonMain/kotlin/Main.kt
@@ -1,5 +1,25 @@
 fun main() {
-    println("Hello, Kotlin ${platform()}!")
+    std()
+}
 
-    println("${getDataClass()}")
+fun std() {
+    println("\n\n\n=== STD ===")
+
+    val ktText = platform()
+    val ktDataClass1 = getDataClass()
+    val ktDataClass1Str = ktDataClass1.toString()
+
+    val ktDataClass2 = DataClass(
+        int = 2,
+        string = "some android string"
+    )
+    val ktDataClass2Int = ktDataClass2.int
+    val ktDataClass2Str = ktDataClass2.string
+
+    println("Hello, $ktText\n$ktDataClass1Str\n" +
+            "DataClass2\n" +
+            "int: $ktDataClass2Int\n" +
+            "string: $ktDataClass2Str"
+    )
+
 }
